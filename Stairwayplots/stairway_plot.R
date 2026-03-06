ab83ebbe1b79caf1714782f28a5c7548f299de09
TheC0der <146098953+TheC0der856@users.noreply.github.com>
2026-03-06 13:13:40 +0100

    Enhance stairway_plOtter with titles and aesthetics
    Updated stairway_plOtter function to include titles for plots and adjusted plot aesthetics.

--- Stairwayplots/stairway_plot.R
+++ Stairwayplots/stairway_plot.R
@@ -1,7 +1,4 @@
-# Stairwayplot my way
 library(ggplot2)
-#library(ggthemes)
-#library(ggpubr)
 library(envalysis)
 library(patchwork)
 
@@ -11,7 +8,7 @@ popmap <- read.csv("popmap_g_h_t_united_MASTER.txt",sep = "\t", header = F)
 pops <- unique(popmap$V2)
 
 
-stairway_plOtter <- function(species, color){
+stairway_plOtter <- function(species, color, title){
   in.file <- list.files(path = paste("./",species,"/",species, sep = ""), pattern = ".final.summary")
   in.data <- read.table(paste("./",species,"/",species,"/",in.file[1], sep = ""), header = TRUE)
   #x_breaks <- c(0,0.01,0.1,0.4,1,2,4,10,20,40,100,200)
@@ -24,6 +21,7 @@ stairway_plOtter <- function(species, color){
                 1,2,4,#6,
                 10,20,40,100,200, 400)
   plot <- ggplot(in.data, aes(x=year)) +
+    labs(title = title) +
     scale_x_continuous(trans = "log1p", 
                        breaks = x_breaks*1000, 
                        labels = x_breaks,
@@ -36,43 +34,44 @@ stairway_plOtter <- function(species, color){
                        labels = y_breaks) +
     ylab("") +
     geom_ribbon(aes(ymin = Ne_2.5., ymax = Ne_97.5.), fill = color, alpha = 0.2) +
-    geom_line(data=in.data, aes(y=Ne_median), colour = color, linetype='solid', size = 3) +
-    geom_line(data=in.data, aes(y=Ne_2.5.), colour = color, linetype='solid', size = .25) +
-    geom_line(data=in.data, aes(y=Ne_97.5.), colour = color, linetype='solid', size = .25) +
+    geom_line(data=in.data, aes(y=Ne_median), colour = color, linetype='solid', size = 1.8) +
+    #geom_line(data=in.data, aes(y=Ne_2.5.), colour = color, linetype='solid', size = .25) +
+    #geom_line(data=in.data, aes(y=Ne_97.5.), colour = color, linetype='solid', size = .25) +
     theme_publish() +
     theme(legend.position = "none",
+          plot.title = element_text(size = 22),
           axis.text.x = element_text(size = 22),
           axis.text.y = element_text(size = 22))
   assign(paste(species,"final",sep="_"), plot, envir = .GlobalEnv)
 }
 
 
-P_t_most_east <- lapply(pops[7], stairway_plOtter, color = "darkslategray")[[1]]
-P_t_little_east <- lapply(pops[9], stairway_plOtter, color = "darkslategray4")[[1]]
-P_t_agua <- lapply(pops[8], stairway_plOtter, color = "darkslategray3")[[1]]
-P_t_west <- lapply(pops[6], stairway_plOtter, color = "darkslategray1")[[1]]
+P_t_most_east <- lapply(pops[7], stairway_plOtter, color = "darkslategray", title = "Lagunetas and east Anaga")[[1]]
+P_t_little_east <- lapply(pops[9], stairway_plOtter, color = "darkslategray4", title = "West Anaga")[[1]]
+P_t_agua <- lapply(pops[8], stairway_plOtter, color = "darkslategray3", title = "Center")[[1]]
+P_t_west <- lapply(pops[6], stairway_plOtter, color = "darkslategray1", title = "West")[[1]]
 
-P_g_majona <- lapply(pops[3], stairway_plOtter, color = "darkolivegreen")[[1]]
-P_g_south <- lapply(pops[2], stairway_plOtter, color = "darkolivegreen3")[[1]]
-P_g_north <- lapply(pops[1], stairway_plOtter, color = "darkolivegreen1")[[1]]
+P_g_majona <- lapply(pops[3], stairway_plOtter, color = "darkolivegreen", title = "East")[[1]]
+P_g_south <- lapply(pops[2], stairway_plOtter, color = "darkolivegreen3", title = "Center")[[1]]
+P_g_north <- lapply(pops[1], stairway_plOtter, color = "darkolivegreen1", title = "North")[[1]]
 
 
-P_h_east <- lapply(pops[5], stairway_plOtter, color = "gold3")[[1]]
-P_h_west <- lapply(pops[4], stairway_plOtter, color = "gold1")[[1]]
+P_h_east <- lapply(pops[5], stairway_plOtter, color = "gold3", title = "East")[[1]]
+P_h_west <- lapply(pops[4], stairway_plOtter, color = "gold1", title = "West")[[1]]
 
 
 # Combine all plots into a single page
 all_plots <- list(P_t_most_east, P_t_little_east, P_t_agua, P_t_west,
                   P_g_majona, P_g_south, P_g_north,
-                  P_h_east, P_h_west )# have to photoshop and move down, switch H & I
+                  P_h_east, P_h_west )
 
 
 
 
 Stairwayplot <- (
   all_plots[[1]] + all_plots[[2]] + all_plots[[3]] + all_plots[[4]] +
-    all_plots[[5]] + all_plots[[6]] + all_plots[[7]] + plot_spacer() +
-    all_plots[[8]] + all_plots[[9]] + plot_spacer() + plot_spacer()
+  all_plots[[5]] + all_plots[[6]] + all_plots[[7]] + plot_spacer() +
+  all_plots[[8]] + all_plots[[9]] + plot_spacer() + plot_spacer()
 ) +
   plot_layout(ncol = 4) +
   plot_annotation(tag_levels = "A") &
